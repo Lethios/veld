924d08a8214426cd4410e83129861ad1a08a1567
Lethios <lethios@protonmail.com>
2026-08-26 15:29:45 +0530

    docs: add documentation for Vec2

--- src/math/vec2.rs
+++ src/math/vec2.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
+/// A 2-dimensional vector.
 #[derive(Clone, Copy, PartialEq)]
 pub struct Vec2 {
     x: f32,
@@ -7,26 +8,31 @@ pub struct Vec2 {
 }
 
 impl Vec2 {
+    /// Creates a new vector.
     #[must_use]
     pub const fn new(x: f32, y: f32) -> Self {
         Self { x, y }
     }
 
+    /// Returns the dot product of `self` and `rhs`.
     #[must_use]
     pub fn dot(self, rhs: Self) -> f32 {
         (self.x * rhs.x) + (self.y * rhs.y)
     }
 
+    /// Returns the length of `self`.
     #[must_use]
     pub fn length(self) -> f32 {
         self.dot(self).sqrt()
     }
 
+    /// Returns the squared length of `self`.
     #[must_use]
     pub fn length_squared(self) -> f32 {
         self.dot(self)
     }
 
+    /// Returns `self` normalized to length 1.0.
     #[must_use]
     pub fn normalize(self) -> Self {
         self / self.length()
