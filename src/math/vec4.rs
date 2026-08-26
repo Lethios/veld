a6aa18cac6d507ae34e512f4404de0d27e62aec8
Lethios <lethios@protonmail.com>
2026-08-26 17:53:08 +0530

    feat: add Vec4 implementation and tests

---
+++ src/math/vec4.rs
@@ -0,0 +1,121 @@
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+/// A 4-dimensional vector.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Vec4 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+impl Vec4 {
+    /// Create a new Vec4.
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
+    }
+
+    /// Returns the dot product of `self` and `rhs`.
+    pub fn dot(self, rhs: Self) -> f32 {
+        (self.x * rhs.x) + (self.y * rhs.y) + (self.z * rhs.z) + (self.w * rhs.w)
+    }
+
+    /// Returns the length of `self`.
+    pub fn length(self) -> f32 {
+        self.dot(self).sqrt()
+    }
+
+    /// Returns the squared length of `self`.
+    pub fn length_squared(self) -> f32 {
+        self.dot(self)
+    }
+
+    /// Returns `self` normalized to length 1.0.
+    pub fn normalize(self) -> Self {
+        self / self.length()
+    }
+}
+
+impl Add<Vec4> for Vec4 {
+    type Output = Self;
+    fn add(self, rhs: Vec4) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+            w: self.w + rhs.w,
+        }
+    }
+}
+
+impl Sub<Vec4> for Vec4 {
+    type Output = Self;
+    fn sub(self, rhs: Vec4) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+            w: self.w - rhs.w,
+        }
+    }
+}
+
+impl Mul<Vec4> for Vec4 {
+    type Output = Self;
+    fn mul(self, rhs: Vec4) -> Self::Output {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+            w: self.w * rhs.w,
+        }
+    }
+}
+
+impl Mul<f32> for Vec4 {
+    type Output = Self;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+            w: self.w * rhs,
+        }
+    }
+}
+
+impl Div<Vec4> for Vec4 {
+    type Output = Self;
+    fn div(self, rhs: Vec4) -> Self::Output {
+        Self {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z,
+            w: self.w / rhs.w,
+        }
+    }
+}
+
+impl Div<f32> for Vec4 {
+    type Output = Self;
+    fn div(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+            w: self.w / rhs,
+        }
+    }
+}
+
+impl Neg for Vec4 {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+            w: -self.w,
+        }
+    }
+}
